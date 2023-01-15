40468d1dc1ea5fc43f096778b578048b660c260c
nusa1890 <imadenusayudiskara@gmail.com>
2023-01-16 00:06:54 +0800

    Refactor code (MoviesViewModel)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,7 @@
         <div class="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-6">
             <ul class="flex flex-col md:flex-row items-center">
                 <li>
-                    <a href="#">
+                    <a href={{ route('movies.index')}}>
                         <svg width='46' height='46' viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><rect width='32' height='32' stroke='none' fill='#000000' opacity='0'/>
                             <g transform="matrix(1.4 0 0 1.4 16 16)" >
                             <path style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255, 255, 255); fill-rule: nonzero; opacity: 1;" transform=" translate(-12, -12)" d="M 4 4 C 2.9 4 2 4.9 2 6 L 2 9.1503906 C 2 9.5653906 2.2378594 9.9705625 2.6308594 10.101562 C 3.4248594 10.367563 4 11.12 4 12 C 4 12.88 3.4248594 13.632437 2.6308594 13.898438 C 2.2378594 14.029438 2 14.434609 2 14.849609 L 2 18 C 2 19.1 2.9 20 4 20 L 20 20 C 21.1 20 22 19.1 22 18 L 22 14.849609 C 22 14.434609 21.762141 14.029437 21.369141 13.898438 C 20.575141 13.632437 20 12.88 20 12 C 20 11.12 20.575141 10.367563 21.369141 10.101562 C 21.762141 9.9705625 22 9.5653906 22 9.1503906 L 22 6 C 22 4.9 21.1 4 20 4 L 4 4 z M 12 6.8007812 L 13.400391 10.099609 L 17 10.400391 L 14.300781 12.800781 L 15.099609 16.199219 L 12 14.400391 L 8.9003906 16.300781 L 9.6992188 12.800781 L 7 10.400391 L 10.599609 10.099609 L 12 6.8007812 z" stroke-linecap="round" />
